3c9c41739ee453eb3ab0507fcbd99710c16c7699
Jan Vlietland <j.vlietland@search4solutions.nl>
2021-09-21 23:52:47 +0200

    Added / in bash script and succesfully tested on Zynthian platform

--- generateBanks.sh
+++ generateBanks.sh
@@ -6,7 +6,7 @@ echo 'generating FluidSynth user banks'
 python3 sf2banks.py 1.rg 2.rg '/zynthian/zynthian-my-data/soundfonts/sf2/' 1 FluidSynth
 
 echo 'generating ZynAddSubFX banks'
-python3 zyn2banks.py 2.rg generated.rg '/zynthian/zynthian-data' 0 ZynAddSubFx
+python3 zyn2banks.py 2.rg generated.rg '/zynthian/zynthian-data/' 0 ZynAddSubFx
 
 
 
